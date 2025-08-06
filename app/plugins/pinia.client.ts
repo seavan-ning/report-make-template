c32294ff5f0243dc5f6079cdcb4eae890809290b
NingSeavan\seavan.ning <seavan093@gmail.com>
2025-08-07 02:38:07 +0800

    update license

--- app/plugins/pinia.client.ts
+++ app/plugins/pinia.client.ts
@@ -1,12 +1,15 @@
 /**
 @license
-[report-make-template] 版权所有 (c) seavan-ning <seavan093@gmail.com>
+[report-make-template] 版权所有 (c)
+   @Author seavan_ning
+   @Email <seavan093@gmail.com>
 基于 MIT 许可证开源（完整协议见项目根目录 LICENSE 文件）
-允许修改/商用，但需保留本声明及作者联系方式
+允许修改/商用，但需保留本声明及作者信息和联系方式
 
 项目地址：
  github：https://github.com/seavan-ning/report-make-template
  gitcode：https://gitcode.com/seavan_ning/report-make-template
+ gitee：https://gitee.com/louisxc/report-make-template
 */
 import { createPersistedState } from 'pinia-plugin-persistedstate'
 
